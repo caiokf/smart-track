4a1b191d301b4d382250abe71754973ce460cae3
caiokf <caiokf@gmail.com>
2011-09-07 19:23:09 -0300

    adding references to functional tests libraries

---
+++ src/SmartTrack.Tests.Acceptance/Features/Measures/add-measures.feature
@@ -0,0 +1,11 @@
+﻿Feature: Add Metrics
+	In order to track my progress
+	As a user
+	I want to be able to add measures to the system
+
+Scenario: Add a new metric
+	Given I am logged in user
+	And I have no existing measures
+	When I add a new measure called "Biceps"
+	Then I can see "Biceps" measure in my home page
+	And I only have 1 measure(s)
---
+++ src/SmartTrack.Tests.Acceptance/Steps/Measures/AddMeasures.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using WatiN.Core;
+
+namespace SmartTrack.Tests.Acceptance.Steps.Measures
+{
+    [Binding]
+    public class AddMeasures
+    {
+        [Given("I am logged in user")]
+        public void GivenIHaveEnteredSomethingIntoTheCalculator()
+        {
+            var driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["ServerAddress"]);
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+
+        [Given("I have no existing measures")]
+        public void WhenIPressAdd()
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [When("I add a new measure called \"(.*)\"")]
+        public void ThenTheResultShouldBe(string measure)
+        {
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then("I can see \"(.*)\" measure in my home page")]
+        public void a(string measure)
+        {
+
+            //ScenarioContext.Current.Pending();
+        }
+
+        [Then("I only have (.*) measure\\(s\\)")]
+        public void b(int measures)
+        {
+
+            //ScenarioContext.Current.Pending();
+        }
+    }
+}
